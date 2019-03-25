2a6f9abf87de192662a5aa3f443554f9ce439662
hly920928 <hly920928@gmail.com>
2019-03-26 00:15:13 +0800

    PAT A1069 The Black Hole of Numbers

--- main.cpp
+++ main.cpp
@@ -1,62 +1,49 @@
 #include <stdio.h>
 #include<algorithm>
-int maxNum=999999;
+#include <string>
 using namespace std;
-int n=0;int L=0;int H=0;int remainN=0;
-struct Student{
-    unsigned int id;
-    unsigned char t;
-    unsigned char v;
-    unsigned char sum;
-    unsigned char type;
-    Student(int _id=0,int _v=0,int _t=0):id(maxNum-_id),t(_t),v(_v),sum(_v+_t){
-          if(v<L||t<L){
-            type=1;
-            remainN--;
-          }else if(v>=H&&t>=H){
-                type=5;
-          }else if(v>=H&&t<H){
-                type=4;
-          }else if(v>=t){
-                type=3;
-          }else   type=2;
-    }
-};
-union StudentData{
-    unsigned long long data;
-    Student student;
-    StudentData(unsigned long long _d=0):data(_d){};
-};
-bool cmp(const Student&a,const Student&b){
-    if(a.type!=b.type)return a.type>b.type;
-    if(a.sum!=b.sum)return a.sum>b.sum;
-     if(a.v!=b.v)return a.v>b.v;
-    return a.id>b.id;
+void toStr(char* str,int i){
+    str[4]='\0';
+     str[3]=i%10+'0';i/=10;
+     str[2]=i%10+'0';i/=10;
+      str[1]=i%10+'0';i/=10;
+      str[0]=i%10+'0';
 }
+int toInt(char* str){
+    int ans=0;
+    ans=(str[0]-'0')*1000+(str[1]-'0')*100+(str[2]-'0')*10+(str[3]-'0');
+    return ans;
+}
+
 int main()
 {
-    //FILE * pFile;pFile = fopen ("./InputData/1062_Talent_and_Virtue.txt","r");fscanf(pFile,"%d %d %d",&n,&L,&H);
-    scanf("%d %d %d",&n,&L,&H);
-    remainN=n;
-    //Student studTable[100010];
-    unsigned long long _studTable[100010];
-    int endID=0;
-    for(int i=0;i<n;i++){
-        int id=0;int v=0;int t=0;
-        //fscanf(pFile,"%d %d %d",&id,&v,&t);
-        scanf("%d %d %d",&id,&v,&t);
-        Student now(id,v,t);
-        if(now.type!=1){
-                StudentData stdt;stdt.student=now;
-            _studTable[endID]=stdt.data;
-            endID++;
-        }
+    int n=0;
+    scanf("%d",&n);
+    char a[5];
+    char b[5];
+    char d[5];
+    toStr(b,n);
+    sort(b,b+4);
+    copy(b,b+4,a);
+    reverse(a,a+4);
+    int diff=toInt(a)-toInt(b);
+
+    if(diff==0){
+        printf("%s - %s = 0000\0",a,b);
+        return 0;
     }
-    //sort(studTable,studTable+endID,cmp);
-       sort(_studTable,_studTable+endID);
-    printf("%d\n",endID);
- Student* studTable=( Student*)_studTable;
- //for(int i=0;i<endID;i++)printf("%d %d %d\n",maxNum-studTable[i].id,studTable[i].v,studTable[i].t);
-  for(int i=endID-1;i>=0;i--)printf("%d %d %d\n",maxNum-studTable[i].id,studTable[i].v,studTable[i].t);
+     toStr(d,diff);
+     int t=0;
+    while(diff!=6174&&t<10){
+        printf("%s - %s = %s\n",a,b,d);
+      toStr(b,diff);
+      sort(b,b+4);
+      copy(b,b+4,a);
+       reverse(a,a+4);
+       diff=toInt(a)-toInt(b);
+       toStr(d,diff);
+       t++;
+    }
+     printf("%s - %s = 6174\n",a,b);
     return 0;
 }
